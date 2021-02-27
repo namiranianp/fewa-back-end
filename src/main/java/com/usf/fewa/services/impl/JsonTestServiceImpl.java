072bb4f7962e8047e2b84f6ce326078426639689
Ian Patrick Murphy <42948100+ianPatrickMurphy@users.noreply.github.com>
2021-02-27 12:57:31 -0800

    Fixed Json Test Formatting Issues

--- src/main/java/com/usf/fewa/services/impl/JsonTestServiceImpl.java
+++ src/main/java/com/usf/fewa/services/impl/JsonTestServiceImpl.java
@@ -7,10 +7,10 @@ public class JsonTestServiceImpl implements JsonTestService {
 	@Override
 	public String returnService() {
 		return "{\r\n"
-				+ "{ \"name\":\"file.txt\", \"type\": \"file\" }\r\n"
-				+ "{ \"name\":\"file.pdf\", \"type\": \"file\" }\r\n"
-				+ "{ \"name\":\"file.mp3\", \"type\": \"file\" }\r\n"
-				+ "{ \"name\":\"/folder\", \"type\": \"directory\" }\r\n"
+				+ "\t{\"name\":\"file.txt\", \"type\": \"file\"},\r\n"
+				+ "\t{\"name\":\"file.pdf\", \"type\": \"file\"},\r\n"
+				+ "\t{\"name\":\"file.mp3\", \"type\": \"file\"},\r\n"
+				+ "\t{\"name\":\"/folder\", \"type\": \"directory\"}\r\n"
 				+ "}";
 	}
 }
