8ac77de1fb4eed31ed2ded2fc9b145fece225d44
Ian Patrick Murphy <42948100+ianPatrickMurphy@users.noreply.github.com>
2021-03-29 14:51:43 -0700

    Changes to FrontEnd demo code for the backend

--- src/main/java/com/usf/fewa/services/impl/TagTestServiceImpl.java
+++ src/main/java/com/usf/fewa/services/impl/TagTestServiceImpl.java
@@ -4,13 +4,19 @@ import com.usf.fewa.services.TagTestService;
 
 public class TagTestServiceImpl implements TagTestService {
 
-	public ArrayList<String> tags = new ArrayList<String>();
+	public ArrayList<String> JPEGtags = new ArrayList<String>();
+	public ArrayList<String> MP4tags = new ArrayList<String>();
+	public ArrayList<String> PDFtags = new ArrayList<String>();
+	public ArrayList<String> PNGtags = new ArrayList<String>();
+	public ArrayList<String> TXTtags = new ArrayList<String>();
 	
 	public TagTestServiceImpl() {
 		
-		tags.add("test1");
-		tags.add("test2");
-		tags.add("test3");
+		JPEGtags.add("jpeg");
+		MP4tags.add("mp4");
+		PDFtags.add("pdf");
+		PNGtags.add("png");
+		TXTtags.add("txt");
 		
 	}
 	
@@ -19,30 +25,179 @@ public class TagTestServiceImpl implements TagTestService {
 		
 		String output = "";
 		
-		if(tag.startsWith("addTag-")) {
+		if(tag.startsWith("jpeg-addTag-")) {
 			
-			if(!tags.contains(tag.substring(7))) {
-				tags.add(tag.substring(7));
+			if(!JPEGtags.contains(tag.substring(7))) {
+				JPEGtags.add(tag.substring(7));
 			}
 			
-		}
-		if(tag.startsWith("deleteTag-")) {
+			output += "{\"tags\":[";
 			
-			if(tags.contains(tag.substring(10))) {
-				tags.remove(tag.substring(10));
+			for(String tagName : JPEGtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("mp4-addTag-")) {
+			
+			if(!MP4tags.contains(tag.substring(7))) {
+				MP4tags.add(tag.substring(7));
 			}
 			
-		}
+			output += "{\"tags\":[";
 			
-		output += "{\"tags\":[";
+			for(String tagName : MP4tags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("pdf-addTag-")) {
+			
+			if(!PDFtags.contains(tag.substring(7))) {
+				PDFtags.add(tag.substring(7));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : PDFtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("png-addTag-")) {
+			
+			if(!PNGtags.contains(tag.substring(7))) {
+				PNGtags.add(tag.substring(7));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : PNGtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("txt-addTag-")) {
+			
+			if(!TXTtags.contains(tag.substring(7))) {
+				TXTtags.add(tag.substring(7));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : TXTtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
 		
-		for(String tagName : tags) {
+		
+		
+		if(tag.startsWith("jpeg-deleteTag-")) {
 			
-			output += "{\"tag\":\"" + tagName + "\"},";
+			if(JPEGtags.contains(tag.substring(10))) {
+				JPEGtags.remove(tag.substring(10));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : JPEGtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
 			
 		}
-		output = output.substring(0, output.length() - 1);
-		output += "]}";
+		if(tag.startsWith("mp4-deleteTag-")) {
+			
+			if(MP4tags.contains(tag.substring(10))) {
+				MP4tags.remove(tag.substring(10));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : MP4tags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("pdf-deleteTag-")) {
+			
+			if(PDFtags.contains(tag.substring(10))) {
+				PDFtags.remove(tag.substring(10));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : PDFtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("png-deleteTag-")) {
+			
+			if(PNGtags.contains(tag.substring(10))) {
+				PNGtags.remove(tag.substring(10));
+			}
+			
+			output += "{\"tags\":[";
+			
+			for(String tagName : PNGtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
+		if(tag.startsWith("txt-deleteTag-")) {
+			
+			if(TXTtags.contains(tag.substring(10))) {
+				TXTtags.remove(tag.substring(10));
+			}
+		
+			output += "{\"tags\":[";
+			
+			for(String tagName : TXTtags) {
+				
+				output += "{\"tag\":\"" + tagName + "\"},";
+				
+			}
+			output = output.substring(0, output.length() - 1);
+			output += "]}";
+			
+		}
 			
 		return output;
 	}
