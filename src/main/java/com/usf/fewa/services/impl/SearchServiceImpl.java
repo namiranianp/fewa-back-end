f4057f16be9767ff7171fba761c77a147f90eb2a
yelangzhidi <yelangzhidi@gmail.com>
2021-05-10 12:13:43 -0700

    Fixed: Search not include invisible files

--- src/main/java/com/usf/fewa/services/impl/SearchServiceImpl.java
+++ src/main/java/com/usf/fewa/services/impl/SearchServiceImpl.java
@@ -1,7 +1,11 @@
 package com.usf.fewa.services.impl;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.usf.fewa.entity.ViewingObject;
 import com.usf.fewa.repository.TagRepository;
 import com.usf.fewa.repository.ViewingObjectRepository;
 import com.usf.fewa.services.SearchService;
@@ -17,12 +21,16 @@ public class SearchServiceImpl implements SearchService {
 
 	@Override
 	public String SearchByFileName(String fileName) {
-		return SeedDirServiceImpl.listToJson(viewingObjectRepository.findByNameContaining(fileName));
+		List<ViewingObject> list = viewingObjectRepository.findByNameContaining(fileName);
+		list.removeIf(e -> !e.isVisible());
+		return SeedDirServiceImpl.listToJson(list);
 	}
 
 	@Override
 	public String SearchByTagName(String tagName) {
-		return SeedDirServiceImpl.listToJson(tagRepository.getByName(tagName).getViewingObjSet());
+		Set<ViewingObject> set = tagRepository.getByName(tagName).getViewingObjSet();
+		set.removeIf(e -> !e.isVisible());
+		return SeedDirServiceImpl.listToJson(set);
 	}
 
 }
