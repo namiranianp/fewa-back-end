106db295d73322d2291754b53eac46a7728fdb1e
georginajcruz <georginajoycruz@gmail.com>
2021-10-22 16:55:33 -0700

    added logging

--- src/main/java/com/usf/fewa/services/impl/DownloadServiceImpl.java
+++ src/main/java/com/usf/fewa/services/impl/DownloadServiceImpl.java
@@ -7,22 +7,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import com.usf.fewa.services.DownloadService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 public class DownloadServiceImpl implements DownloadService {
 
+	private static Logger log = LogManager.getLogger("DownloadServiceImpl");
+
 	@Override
 	public void download(String filename) throws IOException {
         //get destination for file
+		log.info("path = " + filename);
         String home = System.getProperty("user.home");
+		//TODO: should it be Documents??
 		String destination = home + "/Document/" + filename;
 		//Download File
 		try (InputStream inputStream = Files.newInputStream(Path.of(filename))) {
-                  byte[] buffer = inputStream.readAllBytes();
-                  OutputStream outStream = Files.newOutputStream(Path.of(destination));
-                  outStream.write(buffer);
+			log.info("input stream created for path = " + filename);
+			byte[] buffer = inputStream.readAllBytes();
+			log.info("buffer created");
+			//TODO: outstream not being created, catching ioexception
+			OutputStream outStream = Files.newOutputStream(Path.of(destination));
+			log.info("output stream created for destination = " + destination);
+			outStream.write(buffer);
 
-                  IOUtils.closeQuietly(outStream);
+			IOUtils.closeQuietly(outStream);
 		}
 	}
 
