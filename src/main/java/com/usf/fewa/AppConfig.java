6c9b7284f502e128d19057e60d306914f083139f
georginajcruz <georginajoycruz@gmail.com>
2021-11-03 12:08:57 -0700

    minor bug fix

--- src/main/java/com/usf/fewa/AppConfig.java
+++ src/main/java/com/usf/fewa/AppConfig.java
@@ -1,23 +1,12 @@
 package com.usf.fewa;
 
+import com.usf.fewa.services.*;
+import com.usf.fewa.services.impl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-
-import com.usf.fewa.services.PreviewService;
-import com.usf.fewa.services.SearchService;
-import com.usf.fewa.services.SeedDirService;
-import com.usf.fewa.services.TagService;
-import com.usf.fewa.services.TestService;
-import com.usf.fewa.services.DownloadService;
-import com.usf.fewa.services.impl.PreviewServiceImpl;
-import com.usf.fewa.services.impl.SearchServiceImpl;
-import com.usf.fewa.services.impl.SeedDirServiceImpl;
-import com.usf.fewa.services.impl.TagServiceImpl;
-import com.usf.fewa.services.impl.TestServiceImpl;
-import com.usf.fewa.services.impl.DownloadServiceImpl;
 
 @Configuration
 public class AppConfig {
@@ -50,6 +39,9 @@ public class AppConfig {
 	public DownloadService downloadService() {
 		return new DownloadServiceImpl();
 	}
+
+	@Bean
+	public UploadService uploadService() { return new UploadServiceImpl(); }
 
 	@Bean
 	public String userDirLocation() {
