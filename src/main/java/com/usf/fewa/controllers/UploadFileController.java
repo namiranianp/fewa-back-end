fdac4904416def46d75978beb2f216f42a9313b9
Riley Thompson <rileythompson@Rileys-MacBook-Pro.local>
2021-11-19 18:49:03 -0800

    Upload backend communicates with frontend

--- src/main/java/com/usf/fewa/controllers/UploadFileController.java
+++ src/main/java/com/usf/fewa/controllers/UploadFileController.java
@@ -3,16 +3,12 @@ package com.usf.fewa.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.usf.fewa.services.UploadService;
 
 @RestController
@@ -22,19 +18,9 @@ public class UploadFileController {
     @Autowired
     UploadService uploadService;
 
-//	@CrossOrigin(origins = "http://localhost")
-//	@GetMapping(path = "/")
-//	public ResponseEntity uploadAll(@RequestParam(value = "filePath") String filePaths, @RequestParam(value = "uploadDirectory") String uploadDirectory){
-//        List<String> fileDownloadUrls = new ArrayList<>();
-//        Arrays.asList(filePaths)
-//                .stream()
-//                .forEach(filePath -> uploadService.upload(filePath, uploadDirectory));
-//        return ResponseEntity.ok(fileDownloadUrls);
-//    }
-
 	@CrossOrigin(origins = "http://localhost")
-	@GetMapping(path = "/")
-	public ResponseEntity uploadFile(@RequestParam(value = "filePath") String filePath, @RequestParam(value = "uploadDirectory") String uploadDirectory){
-        return uploadService.upload(filePath, uploadDirectory);
+    @GetMapping(path = "/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity uploadFile(MultipartFile file){
+        return uploadService.upload(file);
     }
 }
