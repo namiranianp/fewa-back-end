397d9f76ededccc6c0a01b545c8a75172dc05d95
Riley Thompson <rileythompson@Rileys-MacBook-Pro.local>
2021-11-19 18:52:52 -0800

    Upload back end communicates with front end

--- src/main/java/com/usf/fewa/services/UploadService.java
+++ src/main/java/com/usf/fewa/services/UploadService.java
@@ -1,7 +1,8 @@
 package com.usf.fewa.services;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public interface UploadService {
-    public abstract ResponseEntity upload(String filePath, String uploadDirectory);
+    public abstract ResponseEntity upload(MultipartFile file);
 }
--- src/main/java/com/usf/fewa/services/impl/UploadServiceImpl.java
+++ src/main/java/com/usf/fewa/services/impl/UploadServiceImpl.java
@@ -1,7 +1,5 @@
 package com.usf.fewa.services.impl;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -9,30 +7,23 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.nio.file.Path;
 import java.io.File;
 
-import com.usf.fewa.entity.Owner;
-import com.usf.fewa.entity.ViewingObject;
 import com.usf.fewa.repository.ViewingObjectRepository;
 
 import com.usf.fewa.services.UploadService;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
 
 public class UploadServiceImpl implements UploadService {
-    private static Logger log = LogManager.getLogger("UploadServiceImpl");
-
     @Autowired
     ViewingObjectRepository repository;
 
     @Override
-    public ResponseEntity upload(String filePath, String uploadDirectory){
-        File file = Path.of(filePath).toFile();
-        log.info("path = " + filePath);
-        String fileName = file.getName();
-        log.info("file = " + fileName);
-        ViewingObject vo = new ViewingObject(fileName, filePath, new Owner());
+    public ResponseEntity upload(MultipartFile file){
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
         
-        repository.save(vo);
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(uploadDirectory)
-                .path(vo.getId() + "_" + fileName).path("/db")
+                .path("/files/download/")
+                .path(fileName).path("/db")
                 .toUriString();
         return ResponseEntity.ok(fileDownloadUri);
     }
