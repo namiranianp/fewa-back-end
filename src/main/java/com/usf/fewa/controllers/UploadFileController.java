8d40fceb5f12cd23809ec7edb00affbecf21a128
Riley Thompson <rileythompson@Rileys-MacBook-Pro.local>
2021-12-01 20:29:19 -0800

    Upload multiple files

--- src/main/java/com/usf/fewa/controllers/UploadFileController.java
+++ src/main/java/com/usf/fewa/controllers/UploadFileController.java
@@ -4,29 +4,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.usf.fewa.services.UploadService;
-
 
 @CrossOrigin(origins = "http://localhost")
 @RestController
 public class UploadFileController {
-    private static Logger log = LogManager.getLogger("DownloadFileController");
 
     @Autowired
     UploadService uploadService;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity uploadFile(@RequestBody MultipartFile file){
-        log.info("Part1");
-        return uploadService.upload(file);
+	public ResponseEntity uploadFile(@RequestBody MultipartFile[] files){
+        List<Object> fileDownloadUrls = new ArrayList<>();
+        Arrays.asList(files)
+                .stream()
+                .forEach(file -> fileDownloadUrls.add(uploadService.upload(file).getBody()));
+        return ResponseEntity.ok(fileDownloadUrls);
     }
 }
