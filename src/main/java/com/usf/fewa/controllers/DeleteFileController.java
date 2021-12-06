cc190251106bd7a2b20ada81761c4f6e692e38cc
Patrick Madden <pvmadden@dons.usfca.edu>
2021-12-06 04:14:37 -0800

    hide and delete file functionality

--- src/main/java/com/usf/fewa/controllers/DeleteFileController.java
+++ src/main/java/com/usf/fewa/controllers/DeleteFileController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.usf.fewa.entity.ViewingObject;
 import com.usf.fewa.repository.ViewingObjectRepository;
 
+import java.io.File;
+
 @RestController
 @RequestMapping(value = "delete")
 public class DeleteFileController {
@@ -18,11 +20,28 @@ public class DeleteFileController {
 	ViewingObjectRepository repository;
 
 	@CrossOrigin(origins = "http://localhost")
-	@GetMapping(path = "/")
-	public void delete(@RequestParam(value = "filePath") String filePath) {
+	@GetMapping(path = "/hide")
+	public void hide(@RequestParam(value = "filePath") String filePath) {
 		ViewingObject vo = repository.getByPath(filePath);
 		vo.setInvisible();
 		repository.save(vo);
 	}
 
+	@CrossOrigin(origins = "http://localhost")
+	@GetMapping(path = "/delete")
+	public void delete(@RequestParam(value = "filePath") String filePath) {
+		ViewingObject vo = repository.getByPath(filePath);
+		File file = new File(vo.getPath());
+
+		if(file.delete())
+		{
+			System.out.println(file.getName() + " deleted");
+		}
+		else
+		{
+			System.out.println(file.getName() + " failed");
+		}
+
+		repository.delete(vo);
+	}
 }
